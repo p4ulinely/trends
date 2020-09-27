3bc2d3b219066e8abd44f7e1e07d8667ef128b7b
Paulinely Morgan <paulinelym@gmail.com>
2020-09-27 19:08:47 -0300

    endpoint para gnews

---
+++ src/controllers/NewsController.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+module.exports = {
+    async show(req, res){
+        try {
+            
+            const stringDeBusca = req.params.q
+
+            if (stringDeBusca.length < 2)
+                return res.status(400).json({erros: "string de busca ivalida" })
+
+            console.log(`:: coletando noticias no gnews...`)
+
+            const url_gnews = `https://gnews.io/api/v4/search?q=${stringDeBusca}&token=${process.env.GNEWS_TOKEN}`
+            const requestGNews = await axios.get(url_gnews)
+            
+            console.log(` : request gnes ok!`)
+
+            res.json(requestGNews.data)
+
+        } catch (err) {
+            console.log(err)
+            return res.status(400).json({erros: "ErrorCatch" })
+        }
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const verificacao = require('./../middlewares/auth')
 
 const TrendsController = require('./../controllers/TrendsController')
+const NewsController = require('./../controllers/NewsController')
+
 
 router.get('/trends/:regiao?', verificacao, TrendsController.index)
+router.get('/gnews/:q', verificacao, NewsController.show)
 
 module.exports = router
